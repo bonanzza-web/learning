ad07242a9a99594ff6c9de972ec445e6d07fcbce
Dmitry Ananiev <frukt07@inbox.ru>
2023-01-04 02:12:48 +0300

    hwscript new version

--- hwscript.py
+++ hwscript.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+import os
+import sys
+
+cmdpath = sys.argv[1]
+bash_cmd = ["cd " + cmdpath, "git status"]
+
+result_os = os.popen(' && '.join(bash_cmd)).read()
+
+for result in result_os.split('\n'):
+	#print(result)
+	#print('---')
+	if result.find('modified') != -1:
+		prepare_result = result.replace('\tmodified:    ', '')
+		print(prepare_result)
+for i in range(len(result_os.split('\n'))):
+	if result_os.split('\n')[i] == 'Untracked files:':
+		print("\tThe new file: ",result_os.split('\n')[i+2])
